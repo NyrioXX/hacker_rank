e5f4b404c4959528cfcf0604888ad86f01180449
Felipe Lima <felipe_hlima@hotmail.com>
2023-08-19 17:04:04 -0300

    Pontos no Campeonato

---
+++ pontos_no_campeonato/pontosNoCampeonato.js
@@ -0,0 +1,13 @@
+const resultados = ["V", "E", "V", "E"]
+
+let pontuacao = 0
+
+for (ponto of resultados) {
+    if (ponto == "V") {
+        pontuacao += 3
+    } else if (ponto == "E") {
+        pontuacao += 1
+    }
+}
+
+console.log(pontuacao)
