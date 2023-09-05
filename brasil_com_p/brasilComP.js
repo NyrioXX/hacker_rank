d7fdf204607095347bc3ba6bf36627d839ea3366
Felipe Lima <felipe_hlima@hotmail.com>
2023-09-05 20:13:19 -0300

    Exercicio brasil com p finalizado

--- brasil_com_p/brasilComP.js
+++ brasil_com_p/brasilComP.js
@@ -1,14 +1,14 @@
-const primeiraLetra = "a"
-const segundaLetra = "v"
-const palavras = ["aveia", "manha", "ave"]
+const primeiraLetra = "a";
+const segundaLetra = "v";
+const inputs = ["aveia", "manha", "ave"];
 
-encontrouPalavra = false
-for (let palavra of palavras) {
-    if (palavra[0] == primeiraLetra && palavra[1] == segundaLetra) {
-        console.log(palavra)
-        encontrouPalavra = true
-    }
+encontrouinput = false;
+for (let input of inputs) {
+  if (input[0] == primeiraLetra && input[1] == segundaLetra) {
+    console.log(input);
+    encontrouinput = true;
+  }
 }
-if (!encontrouPalavra) {
-    console.log('NENHUMA')
+if (!encontrouinput) {
+  console.log("NENHUMA");
 }
